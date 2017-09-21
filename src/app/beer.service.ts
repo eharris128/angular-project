df18880de9c652ad1d059d38196e641b3262d641
Evan Harris <echarris@smcm.edu>
2017-09-21 13:13:26 -0400

    Type conversion issues

--- src/app/beer.service.ts
+++ src/app/beer.service.ts
@@ -1,13 +1,49 @@
 import { Injectable } from "@angular/core";
 import { Beer } from "./beer";
-import { BEERS } from "./mock-beers";
 
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+
+import "rxjs/add/operator/retry";
 @Injectable()
 export class BeerService {
-  getBeers(): Promise<Beer[]> {
-    return Promise.resolve(BEERS);
-  }
-  getBeer(id: number): Promise<Beer> {
-    return this.getBeers().then(beers => beers.find(beer => beer.id === id));
+  results: string[];
+
+  constructor(private http: HttpClient) {}
+  getBeers() {
+    interface BeerResponse {
+      results: object;
+    }
+    return (
+      this.http
+        .get<BeerResponse>("http://localhost:8080/beers")
+        // .get<BeerResponse>("https://quiet-lake-53110.herokuapp.com/beers")
+        .retry(3)
+        .subscribe(
+          data => {
+            this.results = [];
+            for (let prop in data) {
+              for (let i = 0; i < data[prop].length; i++) {
+                this.results.push(data[prop][i]);
+              }
+            }
+            console.log("Results: ", this.results);
+          },
+          (err: HttpErrorResponse) => {
+            if (err.error instanceof Error) {
+              console.log("An error occurred.", err.error.message);
+            } else {
+              console.log(
+                `Backend returned code ${err.status}, body was ${err.error}`
+              );
+            }
+          }
+        )
+    );
+    // getBeers(): Promise<Beer[]> {
+    //   return Promise.resolve(BEERS);
+    // }
+    // getBeer(id: number): Promise<Beer> {
+    //   return this.getBeers().then(beers => beers.find(beer => beer.id === id));
+    // }
   }
 }
--- src/app/beers.component.ts
+++ src/app/beers.component.ts
@@ -27,33 +27,6 @@ export class BeersComponent implements OnInit {
 
   ngOnInit(): void {
     this.getBeers();
-    interface BeerResponse {
-      results: object;
-    }
-    this.http
-      .get<BeerResponse>("http://localhost:8080/beers")
-      // .get<BeerResponse>("https://quiet-lake-53110.herokuapp.com/beers")
-      .retry(3)
-      .subscribe(
-        data => {
-          this.results = [];
-          for (let prop in data) {
-            for (let i = 0; i < data[prop].length; i++) {
-              this.results.push(data[prop][i]);
-            }
-          }
-          console.log("Results: ", this.results);
-        },
-        (err: HttpErrorResponse) => {
-          if (err.error instanceof Error) {
-            console.log("An error occurred.", err.error.message);
-          } else {
-            console.log(
-              `Backend returned code ${err.status}, body was ${err.error}`
-            );
-          }
-        }
-      );
   }
 
   onSelect(beer: Beer): void {
