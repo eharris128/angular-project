b6bee2e444261a6473381caa142bdc911152169d
Evan Harris <echarris@smcm.edu>
2017-09-20 22:02:42 -0400

    Functional app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,49 +1,31 @@
-import { NgModule }             from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
- 
-// import { DashboardComponent }   from './dashboard.component';
-// import { HeroesComponent }      from './heroes.component';
-// import { HeroDetailComponent }  from './hero-detail.component';
- 
-const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  // { path: 'dashboard',  component: DashboardComponent },
-  // { path: 'detail/:id', component: HeroDetailComponent },
-  // { path: 'heroes',     component: HeroesComponent }
-];
- 
-@NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
-})
-export class AppRoutingModule {}
 
-// import { NgModule } from "@angular/core";
-// import { RouterModule, Routes } from "@angular/router";
-// import { BeerDetailComponent } from "./beer-detail/beer-detail.component";
-// import { BeersComponent } from "./beers.component";
-// import { DashboardComponent } from "./dashboard/dashboard.component";
-// const routes: Routes = [
-// {
-// path: "beers",
-// component: BeersComponent
-// },
-// {
-// path: "dashboard",
-// component: DashboardComponent
-// },
-// {
-// path: "",
-// redirectTo: "/dashboard",
-// pathMatch: "full"
-// },
-// {
-// path: "detail/:id",
-// component: BeerDetailComponent
-// }
-// ];
-// @NgModule({
-// imports: [ RouterModule.forRoot(routes) ],
-// exports: [ RouterModule ]
-// })
-// export class AppRoutingModule {}
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { BeerDetailComponent } from "./beer-detail/beer-detail.component";
+import { BeersComponent } from "./beers.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+const routes: Routes = [
+  {
+    path: "beers",
+    component: BeersComponent
+  },
+  {
+    path: "dashboard",
+    component: DashboardComponent
+  },
+  {
+    path: "",
+    redirectTo: "dashboard",
+    pathMatch: "full"
+  },
+  {
+    path: "detail/:id",
+    component: BeerDetailComponent
+  }
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { RouterModule }   from '@angular/router';
-
-import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { BeerDetailComponent } from "./beer-detail/beer-detail.component";
@@ -10,9 +7,7 @@ import { BeersComponent } from "./beers.component";
 import { BeerService } from "./beer.service";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 
-// App Routing modules produces error: app-routing.module.ts is not a module.
-
-// import { AppRoutingModule }     from './app-routing.module';
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -21,32 +16,8 @@ import { DashboardComponent } from "./dashboard/dashboard.component";
     BeersComponent,
     DashboardComponent
   ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-     RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: 'detail/:id',
-        component: BeerDetailComponent
-      },
-      {
-        path: 'beers',
-        component: BeersComponent
-      }
-    ])
-  // AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [BeerService],
   bootstrap: [AppComponent]
 })
-
 export class AppModule {}
