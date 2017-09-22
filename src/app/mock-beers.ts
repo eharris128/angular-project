55dbd326b5b9d35bf8aef6aabf4a5c9a1fa2d084
Evan Harris <echarris@smcm.edu>
2017-09-22 11:20:20 -0400

    Adds more mock beers

--- src/app/mock-beers.ts
+++ src/app/mock-beers.ts
@@ -1,7 +1,12 @@
 import { Beer } from './beer';
 
 export const BEERS: Beer[] = [
+  { name: 'Edelstoff', abv: 5.6, style: 'Lager', id: 0},
   { name: 'Modelo Especial', abv: 4.5, style: 'Smooth', id: 1},
   { name: 'Devil Dog', abv: 7.2, style: 'Bitter', id: 2},
-  { name: 'Budweiser', abv: 5.0, style: 'Heavy', id: 3}
+  { name: 'Budweiser', abv: 5.0, style: 'Heavy', id: 3},
+  { name: 'Carolina Blonde', abv: 4.3, style: 'Cream Ale', id: 4},
+  { name: 'Nashville Lager Beer', abv: 4.8, style: 'Lager - American Light', id: 5},
+  { name: 'McSorley\'s Irish Pale Ale', abv: 5.5, style: 'Red Ale - Irish', id: 6},
+  { name: 'Warsteiner Premium Verum', abv: 4.8, style: 'German Pilsner', id: 7}
 ]
